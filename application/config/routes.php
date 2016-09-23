b36b788e2441d52240511de6c5370d051bbf9a95
LeoPeres <leo_ps.12@hotmail.com>
2016-09-22 21:28:30 -0300

    Aicionando algumas funcionalidade CRUD usuários

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,6 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+//Usuarios
 $route['usuarios'] = 'User';
 $route['usuarios/adicionar'] = 'User/adicionar';
 $route['usuarios/editar/(:num)'] = 'User/editar/$1';
+$route['usuarios/excluir/(:num)'] = 'User/excluir/$1';
+$route['usuarios/atualizar'] = 'User/atualizar';
+$route['usuarios/inserir'] = 'User/inserir';
--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,24 +6,52 @@ class User extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->library('twig');
+		$this->load->model('User_model');
+		$this->load->library('user_agent');
 	}
 
 	public function index()
 	{
+		$usuarios = $this->User_model->GetAll();
 		$data['siteTitulo'] = "Usuários";
+		$data['usuarios'] = $usuarios;
 		$this->twig->display('usuario/list', $data);
 	}
 
 	public function adicionar()
 	{
 		$data['siteTitulo'] = "Adicionar Usuário";
+		$data['msg'] = (isset($_GET['msg'])) ? $_GET['msg'] : 0;
 		$this->twig->display('usuario/add', $data);
+	}
+
+	public function inserir()
+	{
+		$usuario = $this->input->post();
+		$this->User_model->Inserir($usuario);
+		redirect($this->agent->referrer()."?msg=1");
 	}
 
 	public function editar($id)
 	{
+		$usuario = $this->User_model->GetById($id);
+		$data['usuario'] = $usuario;
 		$data['siteTitulo'] = "Editar Usuário";
-		$data['user'] = $id;
 		$this->twig->display('usuario/edit', $data);
+	}
+
+	public function atualizar()
+	{	
+		$usuario = $this->input->post();
+		if(is_null($usuario['id']))
+			redirect($this->agent->referrer());
+		$this->User_model->Atualizar($usuario['id'], $usuario);
+		redirect($this->agent->referrer());
+	}
+
+	public function excluir($id)
+	{
+		$this->User_model->Excluir($id);
+		redirect($this->agent->referrer());
 	}
 }
